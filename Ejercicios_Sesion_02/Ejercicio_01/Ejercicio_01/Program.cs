4c071bc220b18070fcc0e5e7d1bdb6524479dcf5
Agus <107086899+Agus0503@users.noreply.github.com>
2023-06-17 20:23:08 -0300

    Add files via upload

---
+++ Ejercicios_Sesion_02/Ejercicio_01/Ejercicio_01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ejercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Ingrese apellido: ");
+            string apellido = Console.ReadLine();
+
+            Console.Write("Ingrese edad: ");
+            int edad = int.Parse(Console.ReadLine());
+
+            Console.Write("¿Sabe programar? (Sí/No): ");
+            string sabeProgramar = Console.ReadLine();
+
+            string mensaje = "Hola " + nombre + " " + apellido;
+
+            mensaje += " Tenes " + edad + " años";
+
+            if (sabeProgramar.ToLower() == "si")
+            {
+                mensaje += " y sabes programar.";
+            }
+            else
+            {
+                mensaje += " y no sabes programar.";
+            }
+
+            Console.WriteLine(mensaje);
+        }
+    }
+}
---
+++ Ejercicios_Sesion_02/Ejercicio_03/Ejercicio_03/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Ejercicio 3
+
+Operadores Determina los operadores para verificar las siguientes condiciones:
+
+Un número es mayor o igual a 18
+
+Un char es ‘a’
+
+Se cumplen dos conciones a la vez (ambas verdaderas)
+
+Se cumple una de dos condiciones a la vez (una true y otra false*/
+using System;
+
+namespace Ejercicio_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Numero mayor o igual a 18
+            int num = 20;
+
+            if (num >= 18)
+            {
+                Console.WriteLine("Se cumple la condicion");
+            }
+
+            //Char == 'a' 
+            char caracter = 'a';
+
+            if (caracter == 'a')
+            {
+                Console.WriteLine("Se cumple la igualdad");
+            }
+
+            //Dos condiciones verdaderas
+            int num2 = 20;
+            int num3 = 30;
+
+            if (num2 > 10 && num3 > 20)
+            {
+                Console.WriteLine("Ambas condiciones son verdaderas");
+            }
+
+            //Una de dos condiciones (true y false)
+            int num4 = 35;
+            int num5 = 24;
+
+            if (num4 > 30 || num5 < 20)
+            {
+                Console.WriteLine("Una de las dos condiciones se cumple");
+            }
+        }
+    }
+}
