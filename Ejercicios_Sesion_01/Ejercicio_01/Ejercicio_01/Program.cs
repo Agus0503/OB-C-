bad002e2a03a43c7db673304e19ef8fe3f1fce96
Agus0503 <agustin.ostua@hotmail.com.ar>
2023-06-17 17:30:10 -0300

    Uploaded Exercises

---
+++ Ejercicios_Sesion_01/Ejercicio_01/Ejercicio_01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ejercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Agus";
+            Console.WriteLine($"Hola {nombre}!");
+        }
+    }
+}
---
+++ Ejercicios_Sesion_01/Ejercicio_02/Ejercicio_02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ejercicio_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime horaActual = DateTime.Now;
+            string hora = horaActual.ToString("HH:mm:ss");
+            Console.WriteLine("Hora actual: " + hora);
+        }
+    }
+}
