6843221b3a85e58f97f3615a79459ecf0134109e
Agus <107086899+Agus0503@users.noreply.github.com>
2023-06-17 22:18:13 -0300

    Add files via upload

---
+++ Ejercicios_Sesion_03/Ejercicio_01/Ejercicio_01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ejercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente("Agus", 1133204587, "calle falsa 123", "email@email.com", true);
+            Console.WriteLine(cliente);
+        }
+
+        public struct Cliente
+        {
+            public Cliente(string nombre, int telefono, string direccion, string email, bool newClient)
+            {
+                nombreCliente = nombre;
+                telefonoCliente = telefono;
+                direccionCliente = direccion;
+                emailCliente = email;
+                nuevoCliente = newClient;
+            }
+
+            public string nombreCliente { get; set; }
+            public int telefonoCliente { get; set; }
+            public string direccionCliente { get; set; }
+            public string emailCliente { get; set; }
+            public bool nuevoCliente { get; set; }
+
+            public override string ToString() => $"{nombreCliente}, {telefonoCliente}, {direccionCliente}, {emailCliente} {nuevoCliente}";
+        }
+
+    }
+
+}
