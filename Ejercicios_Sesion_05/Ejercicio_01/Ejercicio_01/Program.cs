de2ff8dc7d960526d7861ff4ef7fd0a0c62ca1ff
Agus <107086899+Agus0503@users.noreply.github.com>
2023-06-18 22:12:27 -0300

    Add files via upload

---
+++ Ejercicios_Sesion_05/Ejercicio_01/Ejercicio_01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ejercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            double precioSinDescuento = 1500.99;
+            
+            Console.Write("Ingrese nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Ingrese email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Tiene cupon? (Si/No): ");
+            string respuesta = Console.ReadLine();
+
+            if(respuesta.ToLower() == "si")
+            {
+                Console.Write("Ingrese numero de cupon: ");
+                int cupon = int.Parse(Console.ReadLine());
+                double precioConDescuento = precioSinDescuento - (precioSinDescuento * 0.35);
+                Console.WriteLine($"Precio final: {precioConDescuento}");
+            }
+            else Console.WriteLine($"Precio final: {precioSinDescuento}");
+
+        }
+    }
+}
---
+++ Ejercicios_Sesion_05/Ejercicio_02/Ejercicio_02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                      
+            string[] lenguajes = { "C#", "Java", "C++" };
+            int i = 1;
+            
+            foreach(var lenguaje in lenguajes)
+            {
+                Console.WriteLine($"{i}. {lenguaje}");
+                i++;
+            }
+
+            Console.WriteLine("Elija un lenguaje: ");
+            int opcionElegida = int.Parse(Console.ReadLine());
+
+            switch (opcionElegida)
+            {
+                case 1:
+                    Console.WriteLine("Usted eligio -> C#");
+                    break;
+                case 2:
+                    Console.WriteLine("Usted eligio -> Java");
+                    break;
+                case 3:
+                    Console.WriteLine("Usted eligio -> C++");
+                    break;
+            }
+        }
+    }
+}
